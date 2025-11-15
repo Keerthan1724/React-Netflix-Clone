84b54d7e02286310a198ce1210e7fb950007ec3b
Keerthan1724 <devadigakeerthan08@gmail.com>
2025-11-16 00:13:17 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search_icon.svg'
-import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -22,19 +22,19 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-right">
-        <img src={search_icon} alt="" className='icons' />
+        <img src={search_icon} alt="" className="icons" />
         <p>Children</p>
-        <img src={bell_icon} alt="" className='icons' />
+        <img src={bell_icon} alt="" className="icons" />
         <div className="navbar-profile">
-          <img src={profile_img} alt="" className='profile' />
-          <img src={caret_icon} alt=""  />
+          <img src={profile_img} alt="" className="profile" />
+          <img src={caret_icon} alt="" />
           <div className="dropdown">
             <button>Sign out</button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
-import './Login.css'
-import logo from '../../assets/logo.png'
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-
-  const [signState, setSignState] = useState('Sign In');
+  const [signState, setSignState] = useState("Sign In");
 
   return (
-    <div className='login'>
-      <img src={logo} className='login-logo' alt="" />
+    <div className="login">
+      <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
           <button>{signState}</button>
 
           <div className="form-help">
@@ -27,11 +30,33 @@ const Login = () => {
         </form>
 
         <div className="form-switch">
-          {signState=="Sign In"?<p>New to Netflix? <span onClick={() => {setSignState("Sign Up")}}>Sign Up Now</span></p>:<p>Already have account? <span onClick={() => {setSignState("Sign In")}}>Sign In Now</span></p>}
+          {signState == "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
